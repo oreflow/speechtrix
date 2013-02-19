47896f713c5ea6d22db12784d3da6026796c42e1
jesperalvelid <jesperrepsej@gmail.com>
2013-02-19 12:23:17 +0100

    "added block class and some spec"

---
+++ Block.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Speechtrix.DataModel
+{
+    class Block
+    {
+        //klass Block: short ID, bool[4][4][4], för varje block 4 rotationer med 16 rutor
+        short ID; //from 0-6
+        bool[][][] rotations; //each 4 rotations for a block
+
+
+    }
+}
