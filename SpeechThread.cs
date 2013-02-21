35e2b9f0dad75826933019290e0303a53cc99fa5
= <malmstrom9@gmail.com>
2013-02-21 12:10:36 +0100

    added SpeechThread class and file, also references to the System.speech assembly

---
+++ SpeechThread.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+
+namespace Speechtrix
+{
+    class SpeechThread
+    {
+
+        public SpeechThread(Speechtrix callBack)
+        {
+
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+
+            }
+
+        }
+    }
+}
