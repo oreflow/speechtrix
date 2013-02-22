8a373f5182d8c2c847756df8a42d9e58db7e64eb
jesperalvelid <jesperrepsej@gmail.com>
2013-02-22 02:21:43 +0100

    Both logic and graphics works for falling blocks and next block. Moved LogicBlock to own file, and now used throughout large parts of Graphics as method argument. The old (probably unused) versions of the methods now with LogicBlock as argument still remain in the file. Changed to [x,y] standard in all files.

--- Block.cs
+++ Block.cs
@@ -32,17 +32,17 @@ namespace Speechtrix
             {
                 case 0:
                     rotations = new bool[4][,]{ //0
-                        new bool[4,4]{{true,true,false,false},{true,false,false,false},{true,false,false,false},{false,false,false,false}},
-                        new bool[4,4]{{true,true,true,false},{false,false,true,false},{false,false,false,false},{false,false,false,false}},
-                        new bool[4,4]{{false,true,false,false},{false,true,false,false},{true,true,false,false},{false,false,false,false}},
-                        new bool[4,4]{{true,false,false,false},{true,true,true,false},{false,false,false,false},{false,false,false,false}}};
+			            new bool[4,4]{{true,true,true,false},{true,false,false,false},{false,false,false,false},{false,false,false,false}},
+			            new bool[4,4]{{true,false,false,false},{true,false,false,false},{true,true,false,false},{false,false,false,false}},
+			            new bool[4,4]{{false,false,true,false},{true,true,true,false},{false,false,false,false},{false,false,false,false}},
+			            new bool[4,4]{{true,true,false,false},{false,true,false,false},{false,true,false,false},{false,false,false,false}}};
                     break;
                 case 1:
                     rotations = new bool[4][,]{//1
-                        new bool[4,4]{{true,false,false,false},{true,false,false,false},{true,false,false,false},{true,false,false,false}},
-                        new bool[4,4]{{true,true,true,true},{false,false,false,false},{false,false,false,false},{false,false,false,false}},
-                        new bool[4,4]{{true,false,false,false},{true,false,false,false},{true,false,false,false},{true,false,false,false}},
-                        new bool[4,4]{{true,true,true,true},{false,false,false,false},{false,false,false,false},{false,false,false,false}}};
+			            new bool[4,4]{{true,true,true,true},{false,false,false,false},{false,false,false,false},{false,false,false,false}},
+			            new bool[4,4]{{true,false,false,false},{true,false,false,false},{true,false,false,false},{true,false,false,false}},
+			            new bool[4,4]{{true,true,true,true},{false,false,false,false},{false,false,false,false},{false,false,false,false}},
+			            new bool[4,4]{{true,false,false,false},{true,false,false,false},{true,false,false,false},{true,false,false,false}}};
                     break;
                 case 2:
                     rotations = new bool[4][,]{//2
@@ -53,32 +53,31 @@ namespace Speechtrix
                     break;
                 case 3:
                     rotations = new bool[4][,]{//3
-                        new bool[4,4]{{true,false,false,false},{true,true,false,false},{false,true,false,false},{false,false,false,false}},
-                        new bool[4,4]{{false,true,true,false},{true,true,false,false},{false,false,false,false},{false,false,false,false}},
-                        new bool[4,4]{{true,false,false,false},{true,true,false,false},{false,true,false,false},{false,false,false,false}},
-                        new bool[4,4]{{false,true,true,false},{true,true,false,false},{false,false,false,false},{false,false,false,false}}};
+			            new bool[4,4]{{true,true,false,false},{false,true,true,false},{false,false,false,false},{false,false,false,false}},
+			            new bool[4,4]{{false,true,false,false},{true,true,false,false},{true,false,false,false},{false,false,false,false}},
+			            new bool[4,4]{{true,true,false,false},{false,true,true,false},{false,false,false,false},{false,false,false,false}},
+			            new bool[4,4]{{false,true,false,false},{true,true,false,false},{true,false,false,false},{false,false,false,false}}};
                     break;
                 case 4:
                     rotations = new bool[4][,]{//4
-                        new bool[4,4]{{false,true,false,false},{true,true,false,false},{true,false,false,false},{false,false,false,false}},
-                        new bool[4,4]{{true,true,false,false},{false,true,true,false},{false,false,false,false},{false,false,false,false}},
-                        new bool[4,4]{{false,true,false,false},{true,true,false,false},{true,false,false,false},{false,false,false,false}},
-                        new bool[4,4]{{true,true,false,false},{false,true,true,false},{false,false,false,false},{false,false,false,false}}};
+			            new bool[4,4]{{false,true,true,false},{true,true,false,false},{false,false,false,false},{false,false,false,false}},
+			            new bool[4,4]{{true,false,false,false},{true,true,false,false},{false,true,false,false},{false,false,false,false}},
+			            new bool[4,4]{{false,true,true,false},{true,true,false,false},{false,false,false,false},{false,false,false,false}},
+			            new bool[4,4]{{true,false,false,false},{true,true,false,false},{false,true,false,false},{false,false,false,false}}};
                     break;
-
                 case 5:
                     rotations = new bool[4][,]{//5
-                        new bool[4,4]{{false,true,false,false},{true,true,false,false},{false,true,false,false},{false,false,false,false}},
-                        new bool[4,4]{{false,true,false,false},{true,true,true,false},{false,false,false,false},{false,false,false,false}},
-                        new bool[4,4]{{true,false,false,false},{true,true,false,false},{true,false,false,false},{false,false,false,false}},
-                        new bool[4,4]{{true,true,true,false},{false,true,false,false},{false,false,false,false},{false,false,false,false}}};
+			            new bool[4,4]{{false,true,false,false},{true,true,true,false},{false,false,false,false},{false,false,false,false}},
+			            new bool[4,4]{{false,true,false,false},{true,true,false,false},{false,true,false,false},{false,false,false,false}},
+			            new bool[4,4]{{true,true,true,false},{false,true,false,false},{false,false,false,false},{false,false,false,false}},
+			            new bool[4,4]{{true,false,false,false},{true,true,false,false},{true,false,false,false},{false,false,false,false}}};
                     break;
                 case 6:
                     rotations = new bool[4][,]{//6
-                        new bool[4,4]{{true,true,false,false},{false,true,false,false},{false,true,false,false},{false,false,false,false}},
-						new bool[4,4]{{false,false,true,false},{true,true,true,false},{false,false,false,false},{false,false,false,false}},
-                        new bool[4,4]{{true,false,false,false},{true,false,false,false},{true,true,false,false},{false,false,false,false}},
-                        new bool[4,4]{{true,true,true,false},{true,false,false,false},{false,false,false,false},{false,false,false,false}}};
+			            new bool[4,4]{{true,false,false,false},{true,true,true,false},{false,false,false,false},{false,false,false,false}},
+			            new bool[4,4]{{false,true,false,false},{false,true,false,false},{true,true,false,false},{false,false,false,false}},
+			            new bool[4,4]{{true,true,true,false},{false,false,true,false},{false,false,false,false},{false,false,false,false}},
+			            new bool[4,4]{{true,true,false,false},{true,false,false,false},{true,false,false,false},{false,false,false,false}}};
                     break;
                 default:
                     break;
