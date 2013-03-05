4b0523d8d71858af92af496cd55af6ec55fab3b3
= <malmstrom9@gmail.com>
2013-03-05 15:54:10 +0100

    gfx

--- Graphics.cs
+++ Graphics.cs
@@ -17,7 +17,7 @@ namespace Speechtrix
     {
         static bool debug = false;
         private static Surface screen;
-        static bool fullScreen = false;
+        static bool fullScreen = true;
         static int SCREEN_HEIGHT = 720;//(int) System.Windows.SystemParameters.PrimaryScreenHeight;
         static int SCREEN_WIDTH = 1280; //(int) System.Windows.SystemParameters.PrimaryScreenWidth;
         static int blockY;
